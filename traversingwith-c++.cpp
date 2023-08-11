982054c8d9a9d1644d5de4a7dec82f62521e9f44
khandokeromar5574 <ooshati5574@gmail.com>
2023-08-11 18:16:31 +0600

    2no

---
+++ traversingwith-c++.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int k,arr[5]={10 ,20 ,30,40 ,50};
+    int n = 5;
+
+    for(k=0;k<n;k++){
+
+        cout << arr[k] << "\n";
+    }
+
+}
